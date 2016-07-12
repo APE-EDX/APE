14167181dba583975375909e7dc1d6d618153b0d
Blaron <blaronghb@gmail.com>
2016-07-12 18:16:15 +0200

    Add files via upload
    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,54 +1,16 @@
-// Require electron modules
-var {app, BrowserWindow, ipcMain}  = require('electron');
-
+var app = require('electron').app;  // Module to control application life.
+var BrowserWindow = require('electron').BrowserWindow;  // Module to create native browser window.
 // var crashReporter = require('electron').crashReporter;
-var path = require("path");
-var net = require("net");
-
-var dllSocket = null;
-
-var server = net.createServer((socket) => {
-    console.log("CLIENT IN");
-    dllSocket = socket;
-}).on('error', (err) => {
-    throw err;
-});
-
-ipcMain.on('send-code', (event, arg) => {
-    if (dllSocket) {
-        console.log("SENDING");
-        var json = {
-            method: 'eval_js',
-            args: [arg]
-        };
-
-        dllSocket.write(JSON.stringify(json));
-    }
-});
-
-// grab a random port.
-server.listen(25100, 'localhost', () => {
-    address = server.address();
-    console.log('opened server on %j', address);
-
-    var injector = require('../InjectorAddon/build/Release/injector');
-
-    var kernel32Exe = path.resolve(__dirname, "../APEKernel32/Release/APEKernel32.exe");
-    var dllPath = path.resolve(__dirname, "../APEDLL/bin/APEDLL.dll");
-    var targetProcess = "notepad.exe";
-
-    console.log(dllPath);
-    console.log(injector.injectDLL(kernel32Exe, targetProcess, dllPath));
-});
+var path = require("path")
 
 var program = require("commander")
   .option("-d, --dev-tools", "Open Dev Tools on start up")
-  .option("-r, --hard-reloading", "Open Dev Tools on start up")
   .parse(process.argv)
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
 var mainWindow = null;
+var SplashWin = null;
+var TimerSplash = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -56,13 +18,6 @@ app.on('window-all-closed', function() {
     app.quit();
   }
 });
-
-// Live reloading
-let options = {};
-if (program.hardReloading) {
-    options['electron'] = require('electron-prebuilt');
-}
-require('electron-reload')(path.resolve(__dirname, '../dist'), options);
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
@@ -82,7 +37,20 @@ app.on('ready', function() {
   mainWindow.setMenu(null);
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + path.resolve(__dirname, '../public/index.html'));
+  mainWindow.loadURL('file://' + path.resolve(__dirname,'../public/index.html'));
+
+  // Create Splash Screen.
+  SplashWin = new BrowserWindow({width: 400, height: 560, frame: false, resizable: false, algaysOnTop: true });
+  SplashWin.setMenu(null);
+  TimerSplash = setTimeout(CloseSplash, 2000);
+
+  // load the splash.html of the app.
+  SplashWin.loadURL('file://' + path.resolve(__dirname,'../public/splash.html'));
+
+  // close the splash.html of the app.
+  function CloseSplash() {
+    SplashWin.close();
+    }
 
   // Open the devtools.
   program.devTools && mainWindow.openDevTools({detached:true});
