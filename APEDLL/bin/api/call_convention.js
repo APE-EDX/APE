9837ce52b805e60340e186d178d224f6268f99da
gpascualg <gpascualg93@gmail.com>
2016-07-15 21:00:13 +0200

    Move subprojectes to other git repositories

--- APEDLL/bin/api/call_convention.js
+++
@@ -1,7 +0,0 @@
-
-CallConvention = {
-    AUTO: -1,
-    STDCALL: 0,
-    CDECLCALL: 1,
-    FASTCALL: 2
-};
--- APEKernel32/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <Windows.h>
-
-int main() {
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
-    return (int)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
-}
