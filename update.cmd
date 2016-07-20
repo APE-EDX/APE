8dfa8ec6e0afabf4269fc8a5f133ce3eea08e5bb
gpascualg <gpascualg93@gmail.com>
2016-07-20 23:43:01 +0200

    Update InjectorAddon and add updating cmd

---
+++ update.cmd
@@ -0,0 +1,9 @@
+@echo OFF
+SET /P AREYOUSURE=Are you sure, it will delete all changes inside APEDLL, APEKernel, InjectorAddon and jsAPI (Y/[N])?
+if /I "%AREYOUSURE%" == "Y" (
+    git submodule foreach git stash
+    git submodule update --init --recursive
+    git pull --recurse-submodules
+    git submodule update --recursive
+)
+@echo ON
